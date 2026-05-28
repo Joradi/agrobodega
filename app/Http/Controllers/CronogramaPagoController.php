8656bea14fe54b091d979ef6af56c0c1ae616ad4
jonat <jona.diaz.m@gmail.com>
2026-05-28 12:21:29 -0400

    Modelos, controladores y migraciones creados con mcr

---
+++ app/Http/Controllers/CronogramaPagoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CronogramaPago;
+use Illuminate\Http\Request;
+
+class CronogramaPagoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CronogramaPago $cronogramaPago)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CronogramaPago $cronogramaPago)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CronogramaPago $cronogramaPago)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CronogramaPago $cronogramaPago)
+    {
+        //
+    }
+}
